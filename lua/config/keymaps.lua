5bc98be043dd6c5ae1dd4e6739bf51005fb04f74
braydio <chaffee.brayden@gmail.com>
2025-05-07 06:14:19 -0400

    2025-05-07 - keymaps.lua - v1.1.0

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,15 +4,52 @@
 
 -- ~/.config/nvim/lua/config/keymaps.lua
 return {
+  -- === File & Dev Navigation ===
   { '<leader>fx', '<cmd>Telescope find_files<CR>', desc = 'Find Files' },
+  { '<leader>ld', '<cmd>Telescope diagnostics<CR>', desc = 'Diagnostics Telescope' },
+
+  -- === LSP Tools ===
   { '<leader>lr', '<cmd>lua vim.lsp.buf.rename()<CR>', desc = 'Rename Symbol' },
   { '<leader>la', '<cmd>lua vim.lsp.buf.code_action()<CR>', desc = 'Code Action' },
-  { '<leader>ld', '<cmd>Telescope diagnostics<CR>', desc = 'Diagnostics Telescope' },
   { '<leader>lf', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>', desc = 'Format Buffer' },
   { '<leader>lh', '<cmd>lua vim.lsp.buf.hover()<CR>', desc = 'Hover Docs' },
   { '<leader>li', '<cmd>LspInfo<CR>', desc = 'LSP Info' },
+
+  -- === Project Commands ===
   { '<leader>tr', ':!npm run dev<CR>', desc = 'Run Frontend' },
   { '<leader>tb', ':!npm run build<CR>', desc = 'Build Frontend' },
+
+  -- === GPT Agent Switching ===
+  {
+    '<leader>ga',
+    function()
+      require('gp').select_agent 'GPT-4o'
+    end,
+    desc = 'Use GPT-4o',
+  },
+  {
+    '<leader>gm',
+    function()
+      require('gp').select_agent 'GPT-4o Mini'
+    end,
+    desc = 'Use GPT-4o Mini',
+  },
+  {
+    '<leader>g3',
+    function()
+      require('gp').select_agent 'GPT-3.5 Turbo'
+    end,
+    desc = 'Use GPT-3.5 Turbo',
+  },
+
+  -- === Gp.nvim Core Actions ===
   { '<leader>ai', '<cmd>GpChatNew<CR>', desc = 'Ask AI (gp.nvim)' },
-  { '<leader>ac', '<cmd>CodeCompanion<CR>', desc = 'AI Chat (codecompanion.nvim)' },
+  { '<leader>ar', '<cmd>GpChatResp<CR>', desc = 'GPT Respond Again' },
+  { '<leader>at', '<cmd>GpChatToggle<CR>', desc = 'Toggle GPT Chat' },
+  { '<leader>ap', '<cmd>GpChatPaste<CR>', desc = 'Paste GPT Output' },
+
+  -- === Visual Mode AI ===
+  { 'v', '<leader>ae', ":<C-u>'<,'>GpChatNew edit the selection<CR>", desc = 'Edit with GPT' },
+  { 'v', '<leader>ar', ":<C-u>'<,'>GpChatNew rewrite to be simpler<CR>", desc = 'Rewrite Code' },
+  { 'v', '<leader>ac', ":<C-u>'<,'>GpChatNew add concise comments<CR>", desc = 'Comment Code' },
 }

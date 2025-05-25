31c5c1f883b86ece154cab6db3181cc2ad77dddd
braydio <chaffee.brayden@gmail.com>
2025-05-25 12:20:13 -0400

    2025-05-25 - tip.lua - v1.0.0

--- lua/plugins/tip.lua
+++ lua/plugins/tip.lua
@@ -1,20 +1,14 @@
-
-  "TobinPalmer/Tip.nvim",
-  config = function()
-    require("tip").setup({
-      header = "💡 Dev Tip",
-      position = "cmdline", -- "cmdline", "popup", or "notify"
-      delay = 5000,
-      tips = {
-        "Use `:noh` to clear search highlights",
-        "Use `gd` to go to definition",
-        "Use `:lua =expr` to evaluate in place",
-        "Run `:messages` to review past logs",
-        "Call `vim.lsp.buf.hover()` for docs",
-        "💡 Consider wrapping logger.debug in reusable helpers",
-        "🔥 Use Telescope to find TODOs: `:Telescope grep_string search=TODO`"
-      }
-    })
+-- Lazy.nvim
+return {
+  'TobinPalmer/Tip.nvim',
+  event = 'VimEnter',
+  init = function()
+    -- Default config
+    --- @type Tip.config
+    require('tip').setup {
+      seconds = 2,
+      title = 'Tip!',
+      url = 'https://vtip.43z.one', -- Or https://vimiscool.tech/neotip
+    }
   end,
-  event = "VeryLazy"
 }
